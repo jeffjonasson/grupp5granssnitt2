1483c145c99b47498a5050425e68e8924636836d
erikajansson1 <erika_jansson1@hotmail.com>
2018-05-09 14:52:58 +0200

    Navigation system added

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground, Button, Alert } from 'react-native';
+import { createStackNavigator,} from 'react-navigation';
 
-export default class App extends React.Component {
+
+class HomeScreen extends React.Component {
     render() {
 	return (
 		<ImageBackground source={require('./vy2.png')} style={styles.backgroundImage}>
 		
 		<Button
-	    onPress={() => {Alert.alert('Pressed!');
-			   }}
+	    onPress={() => this.props.navigation.navigate('Game')}
 	    title="START GAME"
 	    color="#00e6dc"
 		/>
 
 		<Button
-	    onPress={() => {Alert.alert('Pressed!');
-			   }}
+	    onPress={() => this.props.navigation.navigate('Tutorial')}
 	    title="TUTORIAL"
 	    color="#00e6dc"
 		/>
@@ -26,6 +26,73 @@ export default class App extends React.Component {
 	);
     }
 }
+
+class GameScreen extends React.Component {
+    render() {
+	return (
+	    	<ImageBackground source={require('./vy2.png')} style={styles.backgroundImage}>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		<Text>Game Screen</Text>
+		</View>
+		</ImageBackground>
+	);
+    }
+}
+
+class TutorialScreen extends React.Component {
+    render() {
+	return (
+	    	<ImageBackground source={require('./vy2.png')} style={styles.backgroundImage}>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		<Text>Tutorial Screen</Text>
+		</View>
+		</ImageBackground>
+	);
+    }
+}
+
+const RootStack = createStackNavigator(
+    {
+	Home: HomeScreen,
+	Game: GameScreen,
+	Tutorial: TutorialScreen,
+    },
+    {
+	initialRouteName: 'Home',
+    }
+);
+
+export default class App extends React.Component {
+    render() {
+	return <RootStack />;
+    }
+}
+
+/* export default class App extends React.Component {
+   render() {
+   return (
+   <ImageBackground source={require('./vy2.png')} style={styles.backgroundImage}>
+   
+   <Button
+   onPress={() => {Alert.alert('Pressed!');
+   }}
+   title="START GAME"
+   color="#00e6dc"
+   />
+
+   <Button
+   onPress={() => {Alert.alert('Pressed!');
+   }}
+   title="TUTORIAL"
+   color="#00e6dc"
+   />
+   
+   </ImageBackground>
+
+   
+   );
+   }
+   } */
 
 // 
 
